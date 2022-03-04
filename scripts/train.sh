6d7024d8f3da59732e567d9859fc5a8aa07234b1
mwp-g <99861827+mwp-g@users.noreply.github.com>
2022-03-04 16:26:54 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,13 @@
-dataset=${MTPATH}/math23k \
+dataset=${DMRPATH}/math23k \
 
-mkdir -p ${MTPATH}/ckpt_gen_math23k
+mkdir -p ${DMRPATH}/ckpt_gen_math23k
 
 python3 train.py \
         --eq_src_vocab ${dataset}/eq_src.vocab \
         --wd_src_vocab ${dataset}/wd_src.vocab \
         --tgt_vocab ${dataset}/tgt.vocab \
         --tgt_processed_vocab ${dataset}/tgt.processed.vocab \
-        --retriever ${MTPATH}/ckpt_pre_math23k \
+        --retriever ${DMRPATH}/ckpt_pre_math23k \
         --train_data ${dataset}/train.txt \
         --dev_data ${dataset}/dev.txt \
         --test_data ${dataset}/test.txt \
