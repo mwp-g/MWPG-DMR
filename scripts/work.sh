4b7b6a504cc1a3e0cb94ccfb199b6b433b817627
mwp-g <99861827+mwp-g@users.noreply.github.com>
2022-03-04 16:27:28 +0800

    Update work.sh

--- scripts/work.sh
+++ scripts/work.sh
@@ -1,14 +1,14 @@
-gen_ckpt=${MTPATH}/ckpt_gen_math23k/best.pt
-pre_ckpt=${MTPATH}/ckpt_pre_math23k
-dataset=${MTPATH}/math23k
+gen_ckpt=${DMRPATH}/ckpt_gen_math23k/best.pt
+pre_ckpt=${DMRPATH}/ckpt_pre_math23k
+dataset=${DMRPATH}/math23k
 
-mkdir -p ${MTPATH}/result
+mkdir -p ${DMRPATH}/result
 
 python3 work.py \
         --load_path ${gen_ckpt} \
         --index_path ${pre_ckpt} \
         --test_data ${dataset}/test.txt \
-        --output_path ${MTPATH}/result \
+        --output_path ${DMRPATH}/result \
         --eq_src_vocab_path ${dataset}/eq_src.vocab \
         --wd_src_vocab_path ${dataset}/wd_src.vocab \
         --tgt_vocab_path ${dataset}/tgt.vocab \
