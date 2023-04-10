d9ed80074cb17e4f3d5a079d439b7891d3cc3e82
mwp-g <99861827+mwp-g@users.noreply.github.com>
2023-04-10 09:38:51 +0800

    Update pretrain_tw.sh

--- scripts/pretrain_tw.sh
+++ scripts/pretrain_tw.sh
@@ -2,15 +2,15 @@ dataset=${DMRPATH}/math23k
 
 mkdir -p ${DMRPATH}/ckpt_pre_math23k/tw
 
-python3 pretrain/pretrain_tw.py \
+python3 pretrain_tw.py \
         --eq_src_vocab ${dataset}/eq_src.vocab \
         --wd_src_vocab ${dataset}/wd_src.vocab \
         --tgt_vocab ${dataset}/tgt.vocab \
         --tgt_processed_vocab ${dataset}/tgt.processed.vocab \
         --train_data ${dataset}/train.txt \
-        --dev_data ${dataset}/dev.txt \
-        --per_gpu_train_batch_size 512 \
-        --dev_batch_size 512 \
+        --dev_data ${dataset}/test.txt \
+        --per_gpu_train_batch_size 256 \
+        --dev_batch_size 256 \
         --total_train_steps 20000 \
         --ckpt ${DMRPATH}/ckpt_pre_math23k/tw \
         --gpus 1 \
