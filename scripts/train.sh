1350fd5293dbbe2a5f313ff6dabdacf523256815
mwp-g <99861827+mwp-g@users.noreply.github.com>
2023-04-10 09:39:06 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,13 +7,15 @@ python3 train.py \
         --wd_src_vocab ${dataset}/wd_src.vocab \
         --tgt_vocab ${dataset}/tgt.vocab \
         --tgt_processed_vocab ${dataset}/tgt.processed.vocab \
+        --datasets math23k \
         --retriever ${DMRPATH}/ckpt_pre_math23k \
         --train_data ${dataset}/train.txt \
-        --dev_data ${dataset}/dev.txt \
+        --dev_data ${dataset}/test.txt \
         --test_data ${dataset}/test.txt \
         --ckpt ${DMRPATH}/ckpt_gen_math23k \
+        --MASTER_PORT 55555 \
         --topk 3 \
-        --total_train_steps 50000 \
+        --total_train_steps 100000 \
         --per_gpu_train_batch_size 512 \
         --dev_batch_size 512 \
         --gpus 2 \
