eb85c0aa98edab4b59ce4d031061b726d9216845
mwp-g <99861827+mwp-g@users.noreply.github.com>
2023-04-10 09:38:02 +0800

    Update build_index.sh

--- scripts/build_index.sh
+++ scripts/build_index.sh
@@ -4,14 +4,14 @@ ckpt_folder=${DMRPATH}/ckpt_pre_math23k
 
 # the input_file contains target-side sentences, and pls make sure to remove duplicates
 
-python3 build_index/build_index_eq.py \
+python3 build_index_eq.py \
         --input_file ${dataset}/train.processed.tgt.txt \
         --args_path ${ckpt_folder}/eq_args \
         --ckpt_path ${ckpt_folder}/eq_response_encoder \
         --vocab_path ${dataset}/tgt.processed.vocab \
         --index_path ${ckpt_folder}/eq_mips_index \
 
-python3 build_index/build_index_eq.py \
+python3 build_index_eq.py \
         --input_file ${dataset}/train.processed.tgt.txt \
         --args_path ${ckpt_folder}/eq_args \
         --ckpt_path ${ckpt_folder}/eq_response_encoder \
@@ -19,14 +19,14 @@ python3 build_index/build_index_eq.py \
         --index_path ${ckpt_folder}/eq_mips_index \
         --only_dump_feat
 
-python3 build_index/build_index_tw.py \
+python3 build_index_tw.py \
         --input_file ${dataset}/train.tgt.txt \
         --args_path ${ckpt_folder}/wd_args \
         --ckpt_path ${ckpt_folder}/wd_response_encoder \
         --vocab_path ${dataset}/tgt.vocab \
         --index_path ${ckpt_folder}/wd_mips_index \
 
-python3 build_index/build_index_tw.py \
+python3 build_index_tw.py \
         --input_file ${dataset}/train.tgt.txt \
         --args_path ${ckpt_folder}/wd_args \
         --ckpt_path ${ckpt_folder}/wd_response_encoder \
